d40ff87d538de34ed0f2f7083c4b5facb55f0ba4
amazloumi <y.mazloomi@gmail.com>
2024-10-04 08:43:00 -0400

    update slurm script

--- distributed-mlp/scripts/slurm_single_gpu.sh
+++ distributed-mlp/scripts/slurm_single_gpu.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
-#SBATCH --job-name=mlp-single-gpu
-#SBATCH --output=mlp.out
-#SBATCH --error=mlp.err
-#SBATCH --time=00:10:00
-#SBATCH --partition=kempner
+#SBATCH --job-name=mlp_single_gpu
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=64G
-#SBATCH --account=kempner_dev  # Add your own account here
-#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
 
-module load python
+#SBATCH --time=00:10:00
+#SBATCH --mem=64G
+
+#SBATCH --partition=kempner    # A100
+#SBATCH --account=kempner_dev  # Add your own account here
+
+module load python cuda cudnn
 conda activate dist_computing
 
 python mlp_single_gpu.py
