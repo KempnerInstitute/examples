b0ff8b9a44e05ff57963e58f56ff2b9ce8975da8
naeemkh <khoshnevis.naeem@gmail.com>
2024-10-09 18:33:46 -0400

    address max comment in reordering

--- array-job/run_array_job.sh
+++ array-job/run_array_job.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 #SBATCH --job-name=job-array
+#SBATCH --partition=kempner_requeue
 #SBATCH --account=kempner_dev
-#SBATCH --output=%A_%a.out
 #SBATCH --nodes=1           
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1 
 #SBATCH --cpus-per-task=1
-#SBATCH --time=15:00
 #SBATCH --mem=4GB
-#SBATCH --partition=kempner_requeue
-#SBATCH --array=1-4%2
+#SBATCH --time=15:00
+#SBATCH --array=1-4
+#SBATCH --output=%A_%a.out
 
 module load python
 
